004576538244a05134b5f3f5ab2d769590477b49
Debaditya Mondal <grabapiassistant@gmail.com>
2022-12-01 19:32:08 +0530

    Nothing brother

--- Arrays2/IntroTo2d.java
+++ Arrays2/IntroTo2d.java
@@ -6,7 +6,7 @@ public class IntroTo2d
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[2][3];
+        int[][] arr = new int[3][3];
         for(int i = 0; i < arr.length; i++){
             for(int j = 0; j < arr[i].length; j++){
                 System.out.println("Enter the element at row "+i+" and column "+j+": ");
@@ -17,6 +17,19 @@ public class IntroTo2d
             for(int j = 0; j < arr[i].length; j++){
                 System.out.print(arr[i][j] + ", ");
             }
+        }
+        //column wise input in 2d arrays
+        for(int i = 0; i < arr[0].length; i++){
+            for(int j = 0; j < arr.length; j++){
+                System.out.println("Enter the number in row "+(j+1)+ "column "+(i+1));
+                arr[j][i] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + ", ");
+            }
+            System.out.println();
         }
     }
 }
